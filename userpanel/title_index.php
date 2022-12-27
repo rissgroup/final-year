a5ab8f3af6068916f8a55df32a28a411959136c7
unknown <sabamirza9988@gmail.com>
2022-12-27 14:52:19 +0500

    saba up

--- userpanel/title_index.php
+++ userpanel/title_index.php
@@ -17,7 +17,7 @@
 							<p class="chmsc">Online learning and Earning</p>
 							<h3>
 
-							<p>Learn & Earn</p>
+							<p>Online Learning and Earning</p>
 						
 							</h3>		
 						</div>
@@ -30,7 +30,7 @@
 						<div class="span12">
 						<br>
 								<div class="motto">
-												<p>Learn & Earn:</p>
+												<p>Online Learning and Earning:</p>
 												<p>Boost Up Your Skill with a new way of Learning</p>
 												<p>Best educational plateform that is providing best services to you anywhere anytime.we aim to empower students to change the world with unlock his skills.</p>
 								</div>		
