93933c76c5f7db5339d8ce156b527a0b1c6efe35
unknown <rissgroups@gmail.com>
2023-01-02 12:33:19 +0500

    courses

--- courses.php
+++ courses.php
@@ -11,153 +11,36 @@
     
     <div class="box-container">
     
-      <div class="box">
-          <img src="./img/web.jpg" alt="">
-          <div class="content">
-              <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half"></i>
-              </div>
-              <a href="./web.html" class="title">Web Wevelopment</a>
-              <p>Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications.</p>
-              <div class="info">
-                  <h3> <i class="far fa-clock"></i> 2 hours </h3>
-                  <h3> <i class="far fa-calendar-alt"></i> 3 months </h3>
-                  <h3> <i class="fas fa-book"></i> 12 modules </h3>
-              </div>
+    <?php
+
+$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
+											$course_query = mysqli_query($conn,"select * from courses")or die(mysqli_error());
+											while($row = mysqli_fetch_array($course_query)){
+											$id = $row['id'];
+											?>
+
+<div class="box">
+      <img src="./userpanel/admin/<?php echo $row['course_image']; ?>" alt="">
+      <div class="content">
+          <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star-half"></i>
+          </div>
+          <a href="web.html" class="title"><?php echo $row['course_name']; ?></a>
+          <p><?php echo $row['course_description']; ?></p>
+          <div class="info">
+              <h3> <i class="far fa-clock"></i> <?php echo $row['course_hours']; ?> Hours</h3>
+              <h3> <i class="far fa-calendar-alt"></i> <?php echo $row['course_month']; ?> Months</h3>
+              <h3> <i class="fas fa-book"></i><?php echo $row['course_mod']; ?> Modules</h3>
           </div>
       </div>
-    
-      <div class="box">
-          <img src="./img/APP.jpg" alt="">
-         
-          <div class="content">
-              <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half"></i>
-              </div>
-              <a href="./app.html" class="title">App Development</a>
-              <p>Mobile application development is the process of creating software applications that run on a mobile device, and a typical mobile application utilizes a network connection to work with remote computing resources.</p>
-              <div class="info">
-                  <h3> <i class="far fa-clock"></i> 2 hours </h3>
-                  <h3> <i class="far fa-calendar-alt"></i> 3 months </h3>
-                  <h3> <i class="fas fa-book"></i> 12 modules </h3>
-              </div>
-          </div>
-      </div>
-    
-      
-      <div class="box">
-        <img src="./img/arti.jpg" alt="">
-        
-        <div class="content">
-            <div class="stars">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star-half"></i>
-            </div>
-            <a href="./ai.html" class="title">Artificial Intelligence</a>
-            <p>AI is intelligence demonstrated by machines, as opposed to the natural intelligence displayed by animals and humans. AI research has been defined as the field of study of intelligent agents.</p>
-            <div class="info">
-                <h3> <i class="far fa-clock"></i> 2 hours </h3>
-                <h3> <i class="far fa-calendar-alt"></i> 3 months </h3>
-                <h3> <i class="fas fa-book"></i> 12 modules </h3>
-            </div>
-        </div>
-    </div>
-    
-      <div class="box">
-          <img src="./img/graphic.jpg" alt="">
-          
-          <div class="content">
-              <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half"></i>
-              </div>
-              <a href="./graphic.html" class="title">Graphic Designing</a>
-              <p>Graphic design is a profession, applied art and academic discipline whose activity consists in projecting visual communications intended to transmit specific messages to social groups, with specific objectives.</p>
-              <div class="info">
-                  <h3> <i class="far fa-clock"></i> 2 hours </h3>
-                  <h3> <i class="far fa-calendar-alt"></i> 3 months </h3>
-                  <h3> <i class="fas fa-book"></i> 12 modules </h3>
-              </div>
-          </div>
-      </div>
-    
-      <div class="box">
-          <img src="./img/game.jpeg" alt="">
-         
-          <div class="content">
-              <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half"></i>
-              </div>
-              <a href="./game.html" class="title">Game Development</a>
-              <p>Game Development is the art of creating games and describes the design, development and release of a game. It may involve concept generation, design, build, test and release.</p>
-              <div class="info">
-                  <h3> <i class="far fa-clock"></i> 2 hours </h3>
-                  <h3> <i class="far fa-calendar-alt"></i> 3 months </h3>
-                  <h3> <i class="fas fa-book"></i> 12 modules </h3>
-              </div>
-          </div>
-      </div>
-    
-    
-      <div class="box">
-          <img src="./img/digital.jpeg" alt="">
-          
-          <div class="content">
-              <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half"></i>
-              </div>
-              <a href="./digi.html" class="title">Digital Markiting</a>
-              <p>Digital marketing is the component of marketing that uses the Internet and online based digital technologies such as desktop computers, mobile phones and other digital media and platforms to promote products and services.</p>
-              <div class="info">
-                  <h3> <i class="far fa-clock"></i> 2 hours </h3>
-                  <h3> <i class="far fa-calendar-alt"></i> 3 months </h3>
-                  <h3> <i class="fas fa-book"></i> 12 modules </h3>
-              </div>
-          </div>
-      </div>
-      
-      <div class="box">
-        <img src="./img/content.png" alt="">
-        
-        <div class="content">
-            <div class="stars">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star-half"></i>
-            </div>
-            <a href="./content.html" class="title">Content Writing</a>
-            <p>Content writing is just one content marketing tool in a brand’s arsenal. Pair it with robust research, diligent planning and high-quality visuals to optimize your strategy.</p>
-            <div class="info">
-                <h3> <i class="far fa-clock"></i> 2 hours </h3>
-                <h3> <i class="far fa-calendar-alt"></i> 3 months </h3>
-                <h3> <i class="fas fa-book"></i> 12 modules </h3>
-            </div>
-        </div>
-    </div>
+  </div>
+
+											
+											<?php } ?> 
     
     </div>
     
